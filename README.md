d529a30d26152a227a2f4c9adbe660e705a8c980
jovansurya <jovansurya97@gmail.com>
2023-06-16 03:23:41 +0800

    endpoint

--- README.md
+++ README.md
@@ -6,14 +6,41 @@ http://34.101.127.232/
 ### 1. Users Activity
 |POST| `/register` | To sign up a new user account
 
+Body:
+
+![image](/images/register-bodyy.png)
+
+Response
+![image](/images/register.png)
+
 |POST| `/login` | To login an existing user account
+Body:
+
+![image](/images/login-body.png)
+Response
+![image](/images/login.png)
 
 ### 2.  Articles
 | GET | `/articles` | Get Articles 
+Response:
+![image](/images/artikel.png)
 
 ### 3.  Predict
-| GET | `/harga/<provinsi>` | Get chilly price from 2020-2023  
+| GET | `/harga/<provinsi>` | Get chilly price from 2020-2023
+
+Response
+![image](/images/harga.png)
+
 
 | GET | `/data/<provinsi>` | Get predict chilly price graph
 
+Response
+![image](/images/data.png)
+
+| GET | `/image/health` | Get predict chilly price graph
+Body:
+![image](/images/image-save.png)
+
+Response:
+![image](/images/image.png)
 
--- controller/users.js
+++ controller/users.js
@@ -1,6 +1,10 @@
 const dbuser = require("../models/users").User
 const jwt = require('jsonwebtoken');
 
+const readlineSync = require('readline-sync');
+const bcrypt = require('bcrypt');
+const { response } = require("express");
+
 function generateJwtToken(userId, expiresIn) {
     const payload = {
         sub: userId,
@@ -16,31 +20,39 @@ function generateJwtToken(userId, expiresIn) {
     const token = jwt.sign(payload, secretKey, options);
     return token;
 }
+const saltRounds = 10;
 
 exports.register = async function (req, res) {
     try {
-        const { username, email, password, } = req.body;
+        const { username, email, password } = req.body;
 
-        if (username === undefined || email === undefined || password === undefined) return res.status(400).json({ message: "error minimal masukin lengkap dek" })
-        const isEmail = await dbuser.findOne({
-            email: email
-        });
-        if (isEmail != null) return res.status(400).json({ message: "error udah ada dek" })
+        if (!username || !email || !password) {
+            return res.status(400).json({ message: "Please provide all required fields" });
+        }
+
+        const existingUser = await dbuser.findOne({ where: { email: email } });
+        if (existingUser) {
+            return res.status(400).json({ message: "udah ada dek" });
+        }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
 
         await dbuser.create({
             username: username,
             email: email,
-            password: password
+            password: hashedPassword
         });
+
         res.status(201).json({
-            message: "berhasil", status: 201
-
-        })
+            message: "berhasil bang",
+            status: 201
+        });
     } catch (error) {
-        throw error
+        console.error("Error during user registration:", error);
+        res.status(500).json({ message: "Internal server error" });
     }
-
-}
+};
 
 exports.login = async function (req, res) {
     try {
@@ -50,9 +62,15 @@ exports.login = async function (req, res) {
             email: email
         });
 
-        if (isEmail) {
 
-            if (isEmail.password != password) return res.status(400).json({ message: "password salah" })
+        if (isEmail) {
+            const userPassword = req.body.password; // User-provided password from the login form
+            const storedHashedPassword = isEmail.password;
+            let decrypt = bcrypt.compare(userPassword, storedHashedPassword)
+
+            if (decrypt === true) {
+                return res.status(200).json({ message: "login berhasil" })
+            }
 
         } else {
             return res.status(400).json({ message: "belum regis dek" })
